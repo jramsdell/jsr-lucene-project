1930554a99b4013157cef078bb3349592e029d2c
Vadavim <vadavim@gmail.com>
2018-02-20 11:50:54 -0500

    Working on random walk model.

--- src/main/java/unh/edu/cs/Main.java
+++ src/main/java/unh/edu/cs/Main.java
@@ -170,6 +170,7 @@ public class Main {
                     String queryLocation = args[2];
                     String qrelLocation = args[3];
                     KotlinTrainer trainer = new KotlinTrainer(indexLocation, queryLocation, qrelLocation);
+                    trainer.test();
                 } catch (ArrayIndexOutOfBoundsException e) {
                     printTrainUsage();
                 }
