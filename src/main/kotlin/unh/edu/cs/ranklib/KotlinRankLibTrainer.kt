c7dab1d7e574ad36c0606f92035ec891a17fc034
Vadavim <vadavim@gmail.com>
2018-02-24 10:15:57 -0500

    Trying ranklib

--- src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
+++ src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
@@ -67,10 +67,11 @@ class KotlinRankLibTrainer(indexPath: String, queryPath: String, qrelPath: Strin
             .map { scoreDoc ->
                 val doc = indexSearcher.doc(scoreDoc.doc)
                 val entities = doc.getValues("spotlight")
-                entities.map { TermQuery(Term("text", it.replace("_"," "))) }
-                    .map { BooleanQuery.Builder().add(it, BooleanClause.Occur.SHOULD).build() }
-                    .map { clause -> indexSearcher.explain(clause, scoreDoc.doc).value.toDouble() }
-                    .average()
+                entities.size.toDouble()
+//                entities.map { TermQuery(Term("text", it.replace("_"," "))) }
+//                    .map { BooleanQuery.Builder().add(it, BooleanClause.Occur.SHOULD).build() }
+//                    .map { clause -> indexSearcher.explain(clause, scoreDoc.doc).value.toDouble() }
+//                    .average()
             }
             .toList()
     }
