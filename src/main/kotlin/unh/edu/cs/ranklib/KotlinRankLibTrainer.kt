635a99f74bd8a55c004cd522e084207c811a2533
Vadavim <vadavim@gmail.com>
2018-02-24 09:40:39 -0500

    Trying ranklib

--- src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
+++ src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
@@ -31,14 +31,16 @@ class KotlinRankLibTrainer(indexPath: String, queryPath: String, qrelPath: Strin
 //            .replace("-", " ")
             .replace(replaceNumbers, " ")
             .split(" ")
-            .map { TermQuery(Term("spotlight", it)) }
+            .map { FuzzyQuery(Term("spotlight", it)) }
             .fold(BooleanQuery.Builder(), { acc, termQuery ->
                                             acc.add(termQuery, BooleanClause.Occur.SHOULD) })
             .build()
 
+
         indexSearcher.doc(tops.scoreDocs[0].doc).getValues("spotlight").forEach(::println)
         return tops.scoreDocs
             .map { scoreDoc -> indexSearcher.explain(termQuery, scoreDoc.doc).value.toDouble() }
+            .onEach(::println)
             .toList()
     }
 
