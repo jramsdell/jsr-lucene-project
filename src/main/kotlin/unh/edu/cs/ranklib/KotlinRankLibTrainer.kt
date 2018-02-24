aa63b06336fd5c2d689d3040b652d9de6c8cf6b4
Vadavim <vadavim@gmail.com>
2018-02-24 09:47:16 -0500

    Trying ranklib

--- src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
+++ src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
@@ -1,14 +1,18 @@
 @file:JvmName("KotRankLibTrainer")
 package unh.edu.cs.ranklib
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
 import org.apache.lucene.index.DirectoryReader
 import org.apache.lucene.index.Term
 import org.apache.lucene.search.*
 import org.apache.lucene.store.FSDirectory
 import unh.edu.cs.*
+import java.io.StringReader
 import java.lang.Double.sum
 import java.nio.file.Paths
 import java.util.*
+import kotlin.coroutines.experimental.buildSequence
 
 class KotlinRankLibTrainer(indexPath: String, queryPath: String, qrelPath: String) {
     val indexSearcher = kotlin.run {
@@ -23,21 +27,41 @@ class KotlinRankLibTrainer(indexPath: String, queryPath: String, qrelPath: Strin
     val queryRetriever = QueryRetriever(indexSearcher)
     val queries = queryRetriever.getQueries(queryPath)
     val ranklibFormatter = KotlinRanklibFormatter(queries, qrelPath, indexSearcher)
+    val analyzer = StandardAnalyzer()
+
+    fun createQuery(query: String): BooleanQuery {
+        val tokenStream = analyzer.tokenStream("text", StringReader(query)).apply { reset() }
+
+        val streamSeq = buildSequence<String> {
+            while (tokenStream.incrementToken()) {
+                yield(tokenStream.getAttribute(CharTermAttribute::class.java).toString())
+            }
+            tokenStream.end()
+            tokenStream.close()
+        }
+
+        return streamSeq
+            .map { token -> TermQuery(Term("spotlight", token))}
+            .fold(BooleanQuery.Builder(), { acc, termQuery ->
+                acc.add(termQuery, BooleanClause.Occur.SHOULD)
+            })
+            .build()
+    }
 
     fun addSpotlightSims(query: String, tops: TopDocs): List<Double> {
         val replaceNumbers = """(%\d+|[_-])""".toRegex()
-        val termQuery = query
-//            .replace("_", " ")
-//            .replace("-", " ")
-            .replace(replaceNumbers, " ")
-            .split(" ")
-            .map { FuzzyQuery(Term("spotlight", it), 10) }
-            .fold(BooleanQuery.Builder(), { acc, termQuery ->
-                                            acc.add(termQuery, BooleanClause.Occur.SHOULD) })
-            .build()
+//        val termQuery = query
+////            .replace("_", " ")
+////            .replace("-", " ")
+//            .replace(replaceNumbers, " ")
+//            .split(" ")
+//            .map { TermQuery(Term("spotlight", it))}
+//            .fold(BooleanQuery.Builder(), { acc, termQuery ->
+//                                            acc.add(termQuery, BooleanClause.Occur.SHOULD) })
+//            .build()
+        val termQuery = createQuery(query)
 
 
-        indexSearcher.doc(tops.scoreDocs[0].doc).getValues("spotlight").forEach(::println)
         return tops.scoreDocs
             .map { scoreDoc -> indexSearcher.explain(termQuery, scoreDoc.doc).value.toDouble() }
             .onEach(::println)
