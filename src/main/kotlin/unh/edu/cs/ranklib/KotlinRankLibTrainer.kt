3cd02b9e8869308a7047ba0cc4e110732d1dc6b6
Vadavim <vadavim@gmail.com>
2018-02-26 15:16:12 -0500

    Tweaking training / graph analyzer

--- src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
+++ src/main/kotlin/unh/edu/cs/ranklib/KotlinRankLibTrainer.kt
@@ -151,7 +151,8 @@ class KotlinRankLibTrainer(indexPath: String, queryPath: String, qrelPath: Strin
         println(scount.incrementAndGet())
 
         return mixtures
-            .map { pm -> pm.mixture.entries.sumByDouble { (k, v) -> sinks[k]!! * v * pm.score } }
+//            .map { pm -> pm.mixture.entries.sumByDouble { (k, v) -> sinks[k]!! * v * pm.score } }
+            .map { pm -> pm.mixture.entries.sumByDouble { (k, v) -> v } }
             .toList()
     }
 
